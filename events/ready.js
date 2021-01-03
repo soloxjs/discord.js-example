dfc1a4085b0665436651ce2efc1346b607e6c022
solox-wfd <55507281+solox-wfd@users.noreply.github.com>
2021-01-03 20:36:06 +0100

    Discord.js Example#2

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,8 @@ module.exports = async (ready, client, boats) => {
     console.log(`[STARTED] Bot hochgefahren als ${client.user.username} mit ${client.users.cache.size} Usern`);
     
     let statuses = [
-        `MTB Germany`,
         `-help`,
-        `https://discord.gg/DTBY6Ug2fD`
-    ];
+        `rndm stuff`    ];
     setInterval(function() {
         let status = statuses[Math.floor(Math.random() * statuses.length)];
     client.user.setActivity(status, {type: "STREAMING", url: "https://www.twitch.tv/#"});
